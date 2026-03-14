9685a3857c0a4068d0a3502baef3c9215863d7ca
Abdelrahman Gamal <agmal600@gmail.com>
2026-03-14 17:11:06 +0200

    Developing ModbusClient Module and Adjusting CRC

--- CRC/CRC.hpp
+++ CRC/CRC.hpp
@@ -8,7 +8,7 @@
 #pragma once
 
 #include <cstdint>
-#include <optional>
+//#include <optional>
 #include <vector>
 
 //! This namespace contains functions used for CRC calculation
@@ -17,6 +17,16 @@ namespace MB::CRC
     //! Calculates CRC based on the input buffer - C style
     uint16_t calculateCRC(const uint8_t *buff, std::size_t len);
 
+    uint16_t calculateCRC(const std::vector<uint8_t> &buffer, std::size_t len = 0) 
+{
+    // If len is 0 or exceeds buffer size, use the whole buffer.
+    // If you actually need to support len = 0, use std::numeric_limits<std::size_t>::max()
+    std::size_t bufferLength = (len == 0 || len > buffer.size()) ? buffer.size() : len;
+
+    return calculateCRC(buffer.data(), bufferLength);
+}
+
+/*
     //! Calculate CRC based on the input vector of bytes
     uint16_t calculateCRC(const std::vector<uint8_t> &buffer,std::optional<std::size_t> len = std::nullopt)
     {
@@ -28,5 +38,6 @@ namespace MB::CRC
 
         return calculateCRC(static_cast<const uint8_t *>(&(*buffer.begin())), bufferLength);
     }
+*/
 
 }; // namespace MB::CRC
